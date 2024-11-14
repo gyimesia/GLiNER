09554aa8344f5813db4631d31ccd97e9ec8a6ce9
JiM <noads.papi@gmail.com>
2024-11-14 16:29:12 +0100

    last one :)

--- graphs.py
+++ graphs.py
@@ -11,8 +11,9 @@ def merge_sort_reset(df1, df2, sort_col):
     sorted_df.reset_index(drop=True, inplace=True)
     return sorted_df
 
-directory = 'output/phone/500/'
-directory2 = 'output/phone/refine/'
+'''
+directory = 'output/more_labels/500/'
+directory2 = 'output/more_labels/refine/'
 for filename in os.listdir(directory):
     f1 = os.path.join(directory, filename)
     f2 = os.path.join(directory2, filename)
@@ -20,23 +21,24 @@ for filename in os.listdir(directory):
     df1 = pd.read_csv(f1)
     df2 = pd.read_csv(f2)
     df_merged = merge_sort_reset(df1, df2, 'threshold')
-    df_merged.to_csv('output/phone/' + filename)
+    df_merged.to_csv('output/more_labels/' + filename, index = False)
     #print(df_merged)
 
 '''
-label = 'address'
 
+label = 'phone'
 
+df5 = pd.read_csv("output/more_labels/accuracy500.csv")
+df = pd.read_csv("output/" + label + "/accuracy500.csv")
 
+'''
+for i in df5:
+    if i not in ['threshold','sum']:
+        plt.plot(df5['threshold'], df5[i], label=i)
+'''
 
-df5 = pd.read_csv("output/more_labels/500/accuracy500.csv")
-df5ref = pd.read_csv("output/more_labels/refine/accuracy500.csv")
-
-dfcon = merge_sort_reset(df5, df5ref, 'threshold')
-print(dfcon)
-
-#lt.plot(accuracy5['threshold'], accuracy5[label], label=label, color='green')
-#lt.plot(accuracy['threshold'], accuracy[label], label=label, color='green')
+plt.plot(df5['threshold'], df5[label], label="phone + 4 other", color='green', )
+plt.plot(df['threshold'], df[label], label="only phone", color='blue')
 #plt.plot(recall5['threshold'], recall5[label], label=label, color='brown')
 #plt.plot(recall['threshold'], recall[label], label=label, color='orange')
 #plt.plot(precision5['threshold'], precision5[label], label=label, color='black')
@@ -45,10 +47,11 @@ print(dfcon)
 #plt.plot(recall['threshold'], recall['sum'], label='Recall', color='blue')
 #plt.plot(precision['threshold'], precision['sum'], label='Precision', color='red')
 
-#plt.legend()
-#plt.title('Accuracy')
+plt.xlabel("threshold")
+#plt.ylabel()
+plt.legend()
+plt.title('Accuracy(phone)')
 
 plt.show()
 
 
-'''
